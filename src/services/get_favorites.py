ce2b26be9c2b030449faf601fa26e4155e200221
pavivin <pavivin@yandex.ru>
2021-12-04 01:13:27 +0300

    fav

--- src/services/get_favorites.py
+++ src/services/get_favorites.py
@@ -1,18 +1,51 @@
-
+import uuid
+from pydantic import BaseModel
+from pydantic.class_validators import validator
 from services.db import DB
+from settings import USER_ID
+from datetime import datetime
+
+class GetFavoritesDTO(BaseModel):
+    startup_id: uuid.UUID
+    project_name: str
+    description: str
+    presentation_link: str
+    stage: str
+    study_facility: str
+    user_id: uuid.UUID
+    category_name: str
+    img_link: str
+    slug: str
+    created_at: datetime
+
+    @validator('startup_id', 'user_id')
+    def validate_id(startup_id: uuid.UUID):
+        return startup_id.hex
+
+    @validator('created_at')
+    def validate_date(created_at: datetime):
+        return created_at.isoformat()
 
 
 class GetFavoritesService():
+    def __init__(self) -> None:
+        ...
 
-    async def __call__(self, p):
+    async def __call__(self):
         async def category_db():
             query = f"""
-                insert into public.startup
-                (startup_id, project_name, description, presentation_link, stage, 
-                    study_facility, user_id, category_id, slug, img_link, created_at)
-                values (?, '', '', '', '', '', ?, ?, '', '', '');
+                select s.startup_id, s.project_name, s.description, s.presentation_link, s.created_at,
+	                s.stage, s.study_facility, s.user_id, sc.category_name, s.img_link, s.slug
+                from public.startup s
+                join startup_categories sc 
+                on s.category_id = sc.category_id
+                join favorites f
+                on f.startup_id = s.startup_id
+                where f.user_id = '{USER_ID}'
             """
-            await DB.conn.fetch(query, )
+            result = await DB.conn.fetch(query)
+
+            return list(map(lambda row: GetFavoritesDTO(**row).dict(), result))
 
         return await category_db()
 
---
+++ src/services/insert_showcases.py
@@ -0,0 +1,20 @@
+
+from services.db import DB
+
+
+class GetFavoritesService():
+
+    async def __call__(self, p):
+        async def category_db():
+            query = f"""
+                insert into public.startup
+                (startup_id, project_name, description, presentation_link, stage, 
+                    study_facility, user_id, category_id, slug, img_link, created_at)
+                values (?, '', '', '', '', '', ?, ?, '', '', '');
+            """
+            await DB.conn.fetch(query, )
+
+        return await category_db()
+
+
+insert_favorites_service = GetFavoritesService()
