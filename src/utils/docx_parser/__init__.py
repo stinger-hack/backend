a4a37b4d66874cd4aef252ee8ac11096cf4875b1
pavivin <pavivin@yandex.ru>
2021-12-03 21:52:59 +0300

    15 min

--- src/utils/docx_parser/__init__.py
+++ src/utils/docx_parser/__init__.py
@@ -1,8 +1,20 @@
 
+import docx
 import pathlib
+
 STARTUP_APPLICATION = 'startup_form.docx'
 cur_path = pathlib.Path(__file__).parent.resolve()
 word_path = cur_path.joinpath(pathlib.Path(STARTUP_APPLICATION))
-with open(word_path, 'r') as file:
-    print(file.encoding)
-    print(file.read())
+
+doc = docx.Document(word_path)
+properties = doc.core_properties
+
+def process_field():
+    ...
+
+text = []
+
+for paragraph in doc.paragraphs:
+    text.append(paragraph.text[:-2])
+
+print('\n'.join(text))
